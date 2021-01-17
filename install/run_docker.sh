120842e16803038ba17468110202c9aa90b4e50a
Patrick Roche <patrick@localhost.localdomain>
2021-01-17 17:48:49 -0500

    tweak script

--- install/run_docker.sh
+++ install/run_docker.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run --rm --user=$(id -u):$(id -g) --ipc=host --net=host -e DISPLAY -e QT_X11_NO_MITSHM=1 -v $(pwd):/cosmos ballaerospace/cosmos &
+#docker run --rm --user=$(id -u):$(id -g) --ipc=host --net=host -e DISPLAY -e QT_X11_NO_MITSHM=1 -v $(pwd):/cosmos ballaerospace/cosmos &
+docker run --rm --user=$(id -u):$(id -g) --ipc=host --net=host -e DISPLAY -v $(pwd):/cosmos ballaerospace/cosmos &
